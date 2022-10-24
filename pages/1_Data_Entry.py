c0dc9e277c894f598ee332586639fb7a384bf648
rjcannizzo <rc.info@protonmail.com>
2022-10-24 07:51:58 -0500

    added data entry page code

--- pages/1_Data_Entry.py
+++ pages/1_Data_Entry.py
@@ -1,11 +1,46 @@
 import streamlit as st
+import datetime
+import time
 
-def get_exercise_list():
-    """return a list of unique exercise names"""    
+st.header("Data Entry")
+message_area = st.empty()
+
+def add_record(_date, exercise, reps, weight):
+    # add a record to the database
+    try:
+        client = st.session_state['mongo_client']
+        db = client.exercise.exercise
+        _date = datetime.datetime(_date.year, _date.month, _date.day) 
+        data = dict(date=_date, exercise=exercise, reps=reps, weight=weight)
+        _id = db.insert_one(data)
+    except Exception as e:
+        st.error(e)
+
+    if _id:
+        with message_area:
+            st.success("New record added")
+        # time.sleep(3)
+        # message_area.empty()
+
+def convert_date_to_string(_date):
+    return _date.strftime("%Y-%m-%d")
+
+
+def get_exercise_set():
+    """return a set of unique exercise names"""    
     client = st.session_state['mongo_client']
     exercise = client.exercise.exercise
     raw = exercise.find({}, {'exercise': 1, '_id': 0})
     return { item.get('exercise') for item in raw }
 
-ex_list = get_exercise_list()
-ex_list
+exercise_set = get_exercise_set()
+
+with st.form("data_entry_form"):
+    date = st.date_input('Date')
+    exercise = st.selectbox("Select Exercise", options = exercise_set)
+    reps = st.number_input("Reps", min_value=1)
+    weight = st.number_input("Weight", min_value=1)
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        add_record(date, exercise, reps, weight)
